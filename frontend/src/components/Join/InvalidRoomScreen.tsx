73d44bc33e63c755ce6724a13cdd8fabc4c451f8
Makam Lokesh <makamlokesh.ai@gmail.com>
2026-01-29 20:07:38 +0530

    frontend deployed

--- frontend/src/components/Join/InvalidRoomScreen.tsx
+++ frontend/src/components/Join/InvalidRoomScreen.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function InvalidRoomScreen() {
 	return (
 		<div className="h-screen flex items-center justify-center bg-black p-4">
--- frontend/src/components/Join/LoadingScreen.tsx
+++ frontend/src/components/Join/LoadingScreen.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function LoadingScreen() {
 	return (
 		<div className="h-screen flex items-center justify-center bg-black">
